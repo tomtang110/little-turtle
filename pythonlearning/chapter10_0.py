e51178499afbfe89faaaa62a84bbdff6a9a5a481
tomtang110 <33299522+tomtang110@users.noreply.github.com>
2018-03-07 16:28:39 +1100

    Add files via upload

---
+++ pythonlearning/chapter10_0.py
@@ -0,0 +1,8 @@
+member=['turtle','night','lostload','yijing','sunshine']
+member.insert(1,'88\n')
+member.insert(3,'90\n')
+member.insert(5,'85\n')
+member.insert(7,'90\n')
+member.insert(9,'88\n')
+for each in member:
+    print(each,end='')
